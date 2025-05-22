e4df64c87de06fce79da756358cb947ef8748899
Olle Sannerset <olle.sannerset@cygate.se>
2025-05-22 10:42:42 +0200

    project start

--- modules/gns3_project/main.tf
+++ modules/gns3_project/main.tf
@@ -20,3 +20,29 @@ resource "restapi_object" "project" {
   data = jsonencode(local.post_data)
   id_attribute = "project_id"
 }
+
+resource "restapi_object" "open_project" {
+  #count = var.started ? 1 : 0
+  read_path = "/projects/{id}"
+  path  = "/projects/{id}/open"
+  destroy_path  = "/projects/{id}/stop"
+  #create_path  = "/projects/${var.project_id}/nodes/${restapi_object.gns3_node.id}/start"
+  #destroy_path = "/projects/${var.project_id}/nodes/${restapi_object.gns3_node.id}/stop"
+  update_path = ""
+  destroy_method = "POST"
+  update_method = "POST"
+  ignore_all_server_changes = true
+  #id_attribute              = "node_id"
+  object_id = restapi_object.project.id
+  data = "{}"
+
+  # lifecycle {
+  #   ignore_changes = [
+  #     update_path
+  #   ]
+  #   replace_triggered_by = [
+  #     restapi_object.gns3_node,
+  #     #restapi_object.update_node
+  #   ]
+  # }
+}
