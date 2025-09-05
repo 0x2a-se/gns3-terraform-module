9ee13104c50c74a91af2cbd78fec947353d5f1a0
Olle Sannerset <olle.sannerset@cygate.se>
2025-09-05 10:10:50 +0200

    fixed vars

--- modules/gns3_project/variable.tf
+++ modules/gns3_project/variable.tf
@@ -5,62 +5,74 @@ variable "name" {
 
 variable "scene_height" {
   description = "Height of the scene"
-  type        = optional(number, null)
+  type        = number
+  default = null
 }
-
 variable "scene_width" {
   description = "Width of the scene"
-  type        = optional(number, null)
+  type        = number
+  default = null
 }
-
 variable "show_grid" {
   description = "Show grid"
-  type        = optional(bool, null)
+  type        = bool
+  default = null
 }
-
 variable "auto_close" {
   description = "Auto close"
-  type        = optional(bool, false)
+  type        = bool
+  default = null
 }
-
 variable "auto_open" {
   description = "Auto open"
-  type        = optional(bool, true)
+  type        = bool
+  default = null
+}
+variable "snap_to_grid" {
+  description = "Snap to grid"
+  type        = bool
+  default = null
+}
+variable "zoom" {
+  description = "Zoom"
+  type        = number
+  default = null
+}
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+  default = null
 }
 
 variable "auto_start" {
   description = "Auto start"
-  type        = optional(bool, true)
+  type        = bool
+  default = null
 }
 
-variable "snap_to_grid" {
-  description = "Snap to grid"
-  type        = optional(bool, null)
-}
-
-variable "zoom" {
-  description = "Zoom"
-  type        = optional(number, null)
-}
 
 variable "grid_size" {
   description = "Grid size"
-  type        = optional(number, null)
+  type        = number
+  default = null
 }
 
 variable "drawing_grid_size" {
   description = "Drawing grid size"
-  type        = optional(number, null)
+  type        = number
+  default = null
 }
 
 variable "show_interface_labels" {
   description = "Show interface labels"
-  type        = optional(bool, false)
+  type        = bool
+  default = null
 }
 
 variable "show_layers" {
   description = "Show layers"
-  type        = optional(bool, false)
+  type        = bool
+  default = null
 }
 
 variable "variables" {
